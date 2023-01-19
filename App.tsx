ab4baf1cc091c91f806d42a7f0b9de3986c19e73
abrojodigital <abrojodigital@gmail.com>
2023-01-18 21:58:57 -0300

    Completo todo lo que puedo

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import './style.css';
 
 export default function App() {
   const [contador, setContador] = useState(0);
+  const [ahora, setAhora] = useState(new Date(Date.now()).toUTCString());
 
   return (
-      <div>
-        <p>Cliqueaste {contador} veces</p>
-        <p>La  {contador} veces</p>
-        <button onClick={() => setContador(contador + 1)}>Cliqueame</button>
-      </div>
-    );
+    <div>
+      <p>Cliqueaste {contador} veces</p>
+      <p>El último clic fue el: {ahora}</p>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+          setAhora(new Date(Date.now()).toUTCString());
+        }}
+      >
+        Cliqueame
+      </button>
+    </div>
+  );
 }
